47dbf1dae97c393e31caf9400caaacf9186472b9
Ch.Sai Sri Snigdha <snigdhachirumamilla@gmail.com>
2026-04-06 22:58:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -423,7 +423,7 @@ def test():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
-
+    
 # from flask import Flask, request, jsonify, session
 # from flask_cors import CORS
 # import mysql.connector
